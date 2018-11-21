a09dcdcb37d232cf56f0346444073a41c07f0144
TYJ <420785022@qq.com>
2018-11-21 11:19:27 +0800

    pdf 浏览

---
+++ src/main/java/com/im/controller/PdfController.java
@@ -0,0 +1,23 @@
+package com.im.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Created by vostor on 2018/11/21.
+ */
+@Controller
+public class PdfController {
+    private final static Logger logger = LoggerFactory.getLogger(PdfController.class);
+
+    @GetMapping(value = "/{name}.pdf")
+    public String index(@PathVariable("name") String name, Model model) {
+        logger.warn("fileName={}", name);
+        model.addAttribute("filePath", "img/" + name + ".pdf");
+        return "pdf";
+    }
+}
